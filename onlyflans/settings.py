e993f03c856982c36373cc75b7471f2bbe03d213
Sebastian <sebcisternas@gmail.com>
2024-05-19 20:54:22 -0400

    Edit

--- onlyflans/settings.py
+++ onlyflans/settings.py
@@ -30,7 +30,7 @@ DEBUG = True if os.getenv('DEBUG')=='True' else False
 
 
 ALLOWED_HOSTS = ['Proyecto-Only-Flans.onrender.com','*']
-CSRF_TRUSTED_ORIGINS = ['https://localhost:8000']
+
 
 
 # Application definition
@@ -139,7 +139,7 @@ TIME_ZONE = 'UTC'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
